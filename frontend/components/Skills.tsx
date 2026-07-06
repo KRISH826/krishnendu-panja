c9dcff14af68f940ab50218b4f9685d5c76cf24b
KRISH826 <krishnendupanja98@gmail.com>
2026-07-06 20:54:56 +0530

    feat: implement skill interaction component and add technical stack icons

---
+++ frontend/components/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SkillSection = () => {
+    return (
+        <div className='skills-section'>
+
+        </div>
+    )
+}
+
+export default SkillSection
---
+++ frontend/components/ui/components/SkillInteraction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SkillInteraction = () => {
+    return (
+        <div>SkillInteraction</div>
+    )
+}
+
+export default SkillInteraction
